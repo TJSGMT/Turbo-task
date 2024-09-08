9c07d5f24e5ab9b2e82d9a3938ea211c450e33a3
tjsgmt <sorecognac@gmail.com>
2024-09-08 16:32:27 +0530

    removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Disable bootsnap cache in production
-  config.bootsnap.cache = false
-
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
